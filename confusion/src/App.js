453a5544e5474b47cbbd067b45d7460c213de873
Le Huynh Minh Tri <lehuynhminhtri09082003@gmail.com>
2024-01-12 15:19:26 +0700

    part1

---
+++ confusion/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import React from 'react';
+import Navigation from  './components/Navigation';
+import Player from './components/Players';  
+import Footer from './components/Footer';
+function App() {
+  return (
+    <div className="App">
+      
+      <Navigation />
+       <Player />
+       <Footer /> 
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ confusion/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        {}
+        
+        {/* Footer */}
+        <div className="footer">
+          <p>&copy; cong ty gai goi toan Chau A.</p>
+          <p>
+            Designed by Tri chim to 
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
